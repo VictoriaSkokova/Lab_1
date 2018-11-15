03e16e8b0bb13bf17ca32b7637cc7c8760851dcd
VictoriaSkokova <44588083+VictoriaSkokova@users.noreply.github.com>
2018-11-15 23:16:36 +0300

    Revert "Файл"
    This reverts commit e5d6da6ccdb1926b8c64f96b8f5f765284818116.

--- Function.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-int put_array(size_t ** array, const size_t height, const size_t width, const size_t number);
-void print_array(size_t ** array, const size_t height, const size_t width, const size_t k);
-bool equals(const size_t* array1, const size_t array1_size, size_t ** array2, const size_t height, const size_t width);
